abf1a146f6e73dba0bf58027756da3360ae30876
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-10 23:15:28 -0700

    Configure application.rb to not generate helpers, tests, or assets for each generate[A

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,16 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module ReferMeProject
   class Application < Rails::Application
+
+    config.generators do |g|
+       g.helper_specs false
+       g.test_framework false
+       g.assets false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
