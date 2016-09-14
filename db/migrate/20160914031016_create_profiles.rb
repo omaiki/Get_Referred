8744723fa60f3e0428b66518de545509f4013d6f
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-13 20:33:41 -0700

    Created form partial for profiles, profile migration

--- db/migrate/20160914031016_create_profiles.rb
+++ db/migrate/20160914031016_create_profiles.rb
@@ -1,8 +1,17 @@
 class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :profiles do |t|
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :phone
+      t.string :image
+      t.text :bio
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
