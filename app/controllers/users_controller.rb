465ff11735f7dc1609c62d8f62b536dfe433216d
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-13 20:21:43 -0700

    Add users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
 end
