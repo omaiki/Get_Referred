9c2f82a020260104fb70704201fdc539251b3d7d
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 10:03:13 -0700

    Create assocations for event, friendship, message models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,6 @@
 class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, :class_name => "User"
+  has_many :messages
+  has_many :events
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
 class Message < ApplicationRecord
+  belongs_to :friendship
+  belongs_to :user
+  has_many :responses
+
+  # takes the time created_at and converts it to pacific standard time and then into a readable date and time
+  def created
+   created_at.in_time_zone("Pacific Time (US & Canada)").strftime('%m-%d-%Y %I:%M')
+  end
 end
