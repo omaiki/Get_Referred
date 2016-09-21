fb79d0dcf57fecabb612abb678699ce402ebd900
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 10:12:01 -0700

    Add associations for response and user model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,12 @@
 class Response < ApplicationRecord
+
+  belongs_to :message
+
+  default_scope {order('created_at DESC')}
+
+    # takes the time created_at and converts it to pacific standard time and then into a readable date and time
+  def created
+   created_at.in_time_zone("Pacific Time (US & Canada)").strftime('%m-%d-%Y %I:%M')
+  end
+
 end
