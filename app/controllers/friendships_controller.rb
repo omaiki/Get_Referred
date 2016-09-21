3f5b581026d3b13ccda8f112e0f469b15175ef52
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 11:24:49 -0700

    Complete friendships controller action functionality: crate, destroy, edit, update

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,33 @@
 class FriendshipsController < ApplicationController
-  def create,
+  include FriendshipsHelper
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Added friend."
+      friend_request
+      redirect_to dashboard_index_path
+    else
+      flash[:error] = "Unable to add friend."
+      redirect_to dashboard_index_path
+    end
   end
 
-  def destroy,
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    friend_request_ignore
+    flash[:notice] = "Friendship request ignored"
+    redirect_to dashboard_index_path
   end
 
-  def edit,
+  def edit
+    @friendship = Friendship.find(params[:id])
   end
 
   def update
+    friend_request_accept
+    flash[:notice] = "Accepted friendship."
+    redirect_to dashboard_index_path
+
+
   end
 end
