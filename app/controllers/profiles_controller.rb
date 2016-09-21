fe8def476d52ed3a653447a4d3b4f4a3a4c13342
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 11:26:56 -0700

    Update profiles controller action functionality: include helper, add private sorting methods

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,29 @@
 class ProfilesController < ApplicationController
 
+  include ProfilesHelper
+
+  helper_method :sort_column, :sort_direction
+
   def index
-    @profiles = Profile.all
-    render :index
+    # @user = User.find(session[:user_id])
+    # @profile = @user.profile
+
+    if params[:search]
+      @profiles = Profile.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 15, :page => params[:page])
+    else
+      @profiles = Profile.all.order(sort_column + " " + sort_direction).paginate(:per_page => 15, :page => params[:page])
+    end
   end
 
   def show
-    @profile = Profile.find(params[:id])
+    # @user = User.find(session[:user_id])
+    # @profile = @user.profile
+    @messages = Message.all
+    @profile=Profile.find(params[:id])
+    @current_user = current_user
+    @friendships = Friendship.all
+    @profiles = Profile.all
+    @users = User.all
     render :show
   end
 
@@ -16,30 +33,39 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    profile_params = params.require(:profile).permit(:user_id, :firstname, :lastname, :address, :city, :state, :phone, :image, :bio)
+    profile_params = params.require(:profile).permit(:user_id, :firstname, :lastname, :role, :city, :state, :linkedin, :image, :bio, :seeker, :company, :department, :skills)
     @profile = Profile.create(profile_params)
-    if @profile.save
-      redirect_to @profile
-    else
-      render :new
-    end
+    add_default_image @profile
+    render :new
   end
 
   def edit
-    @profile = Profile.find(params[:id])
+    @user = User.find(session[:user_id])
+    @profile = @user.profile
     render :edit
   end
 
   def update
     @profile = Profile.find(params[:id])
-    updated_attributes = params.require(:profile).permit(:user_id, :firstname, :lastname, :address, :city, :state, :phone, :image, :bio)
+    updated_attributes = params.require(:profile).permit(:user_id, :firstname, :lastname, :role, :city, :state, :linkedin, :image, :bio, :seeker, :company, :department, :skills)
     @profile.update_attributes(updated_attributes)
+    add_default_image @profile
     redirect_to @profile
   end
 
   def destroy
     Profile.destroy(params[:id])
     redirect_to "/"
+  end
+
+  private
+
+  def sort_column
+    Profile.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
 
