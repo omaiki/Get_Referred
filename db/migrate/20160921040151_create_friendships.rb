b277285438113b620dd242425a4714edb1ffd95a
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-20 21:17:33 -0700

    Add columns for friendships migration table

--- db/migrate/20160921040151_create_friendships.rb
+++ db/migrate/20160921040151_create_friendships.rb
@@ -1,8 +1,11 @@
 class CreateFriendships < ActiveRecord::Migration[5.0]
   def change
     create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+      t.string :status
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
