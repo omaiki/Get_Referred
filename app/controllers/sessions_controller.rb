4c70ef6986771c7063ce216d9182ca094f215273
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 11:29:43 -0700

    Complete sessions controller action functionality: create, destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
-  def new,
+  def new
   end
 
-  def create,
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      login(user)
+      redirect_to dashboard_index_path
+    else
+      flash.now.alert = "Email or password not recognized"
+      render "new"
+    end
   end
 
   def destroy
+    logout
+    redirect_to root_path
   end
 end
