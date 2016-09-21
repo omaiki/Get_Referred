ac42753200d01d27d1e3cd410cd14d896c51f813
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 10:08:13 -0700

    Add search and fullname methods to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,19 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :employments
+  has_many :messages
+
+
+  def full_name
+    "#{firstname} #{lastname}"
+  end
+
+  def self.search(search)
+    if search
+      where('city ILIKE ? OR firstname ILIKE ? OR lastname ILIKE ? OR role ILIKE ? OR company ILIKE?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
+
 end
