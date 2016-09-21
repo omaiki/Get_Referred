618a323069c68e1191f3d6e38bedbfe18f4c2a59
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-21 10:27:04 -0700

    Generate dashboard controller, create sessions, users helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 
 
+  get 'dashboard/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
