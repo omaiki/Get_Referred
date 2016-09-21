802277ad745388694ad029106c662b8e79d10f21
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-20 21:21:09 -0700

    Add columns for event migration table

--- db/migrate/20160921040134_create_events.rb
+++ db/migrate/20160921040134_create_events.rb
@@ -1,8 +1,14 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
+      t.integer :friendship_id
+      t.datetime :date
+      t.string :status
+      t.string :location
+      t.float :longitude
+      t.float :latitude
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
