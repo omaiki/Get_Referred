b2f974ff0cd3a839d7fbe9d605417e6ecf7dcbda
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-22 19:01:50 -0700

    Add rails_12factor gem for heroku deployment asset error

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module ReferMeProject
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # config/application.rb
-config.assets.initialize_on_precompile = false
+
   end
 end
