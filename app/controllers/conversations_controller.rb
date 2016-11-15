5c94dd7e99f954194d8c9ff1bdfc6b1600d28c5b
Oshoke Maiki <oshokemaiki@gmail.com>
2016-11-14 19:24:15 -0800

    Add before_action to conversation controller and redirect to app controller if no record found

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,13 @@
 class ConversationsController < ApplicationController
 
   before_action :get_mailbox
+  before_action :get_conversation, except: [:index]
 
   def index
     @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
   end
 
   private
