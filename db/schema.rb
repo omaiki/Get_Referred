51b58f467150e2ec3dad7aa8e38c1660aa34f961
Oshoke Maiki <oshokemaiki@gmail.com>
2016-11-25 02:18:14 -0800

    Add form to choose file for upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114090823) do
+ActiveRecord::Schema.define(version: 20161125100913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,8 +136,12 @@ ActiveRecord::Schema.define(version: 20161114090823) do
     t.text     "work_history"
     t.text     "portfolio"
     t.text     "criteria_question"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "responses", force: :cascade do |t|
