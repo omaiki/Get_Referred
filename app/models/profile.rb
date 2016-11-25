b24d0d03c2779da102c2b65a5f60da44e7862744
Oshoke Maiki <oshokemaiki@gmail.com>
2016-11-25 02:09:39 -0800

    Move paperclip functionality to profile model, run profile avatar paperclip migration

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,11 @@
 class Profile < ApplicationRecord
+  has_attached_file :avatar, styles: { large: "600x600>",medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   belongs_to :user
   has_many :employments
   has_many :messages
+
 
 
   def full_name
